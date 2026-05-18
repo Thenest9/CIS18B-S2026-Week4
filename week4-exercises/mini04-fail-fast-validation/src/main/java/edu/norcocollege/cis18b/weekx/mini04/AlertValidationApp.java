f18ff026817ab23cc1234e6a7e3e1263b9593a90
Thenest9 <irigoyene20@gmail.com>
2026-05-18 08:52:45 -0700

    Completed Mini04 Assignment

--- week4-exercises/mini04-fail-fast-validation/src/main/java/edu/norcocollege/cis18b/weekx/mini04/AlertValidationApp.java
+++ week4-exercises/mini04-fail-fast-validation/src/main/java/edu/norcocollege/cis18b/weekx/mini04/AlertValidationApp.java
@@ -8,6 +8,21 @@ public class AlertValidationApp {
         Alert invalidAlert = new Alert(2, "   ", null);
 
         // TODO: Validate the valid alert and print a success message.
+        try{
+        validator.validate(validAlert);
+        System.out.println("Sucess");
+        } catch (Exception ex){
+            System.out.println("Failure: " + ex.getMessage());
+        }
+
         // TODO: Validate the invalid alert and print the exception message.
+
+        try{
+            validator.validate(invalidAlert);
+            System.out.println("Invalid Alert");
+        } catch (Exception ex){
+            System.out.println("Error: " + ex.getMessage());
+        }
+
     }
 }
--- week4-exercises/mini04-fail-fast-validation/src/main/java/edu/norcocollege/cis18b/weekx/mini04/AlertValidator.java
+++ week4-exercises/mini04-fail-fast-validation/src/main/java/edu/norcocollege/cis18b/weekx/mini04/AlertValidator.java
@@ -3,7 +3,19 @@ package edu.norcocollege.cis18b.weekx.mini04;
 public class AlertValidator {
     public void validate(Alert alert) throws InvalidAlertException {
         // TODO: Throw if alert is null.
+        if(alert==null)
+        {
+            throw new InvalidAlertException("Alert cannot be null");
+        }
         // TODO: Throw if alert message is null or blank.
+        if(alert.getMessage()==null || alert.getMessage().isBlank())
+        {
+            throw new InvalidAlertException("Message cannot be null or blank");
+        }
         // TODO: Throw if alert level is null.
+        if(alert.getLevel() == null)
+        {
+            throw new InvalidAlertException("Alert cannot be null");
+        }
     }
 }
